c545cf164d0b80c13fa5aa6ce57edf2f5d57668e
josdem <joseluis.delacruz@gmail.com>
2023-07-25 08:05:21 -0400

    #6 Adding test

---
+++ src/test/java/com/josdem/catcher/controller/StatusControllerTest.java
@@ -0,0 +1,25 @@
+package com.josdem.catcher.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@Slf4j
+@SpringBootTest
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class StatusControllerTest {
+
+  private final WebTestClient webTestClient;
+
+  @Test
+  @DisplayName("it stores status")
+  void shouldStoreStatus(TestInfo testInfo) {
+    log.info("Running: {}", testInfo.getDisplayName());
+    webTestClient.post().uri("/catcher/sku/100").exchange().expectStatus().isOk();
+  }
+}
