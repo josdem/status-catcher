fd4acf0f69ea06abf46aafab1a755cb8bcd29ebb
josdem <joseluis.delacruz@gmail.com>
2023-08-23 14:48:59 -0400

    #10 Adding interceptor flag

--- src/main/java/com/josdem/catcher/config/InterceptorFilter.java
+++ src/main/java/com/josdem/catcher/config/InterceptorFilter.java
@@ -1,6 +1,7 @@
 package com.josdem.catcher.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
@@ -11,15 +12,25 @@ import reactor.core.publisher.Mono;
 @Component
 public class InterceptorFilter implements WebFilter {
 
+  @Value("${interceptor.enabled}")
+  private boolean interceptorEnabled;
+
   @Override
   public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
     return chain
         .filter(exchange)
         .doFinally(
             request -> {
-              exchange.getRequest().getHeaders().forEach((key, value) -> log.info("header: {} - {}", key, value));
-              exchange.getAttributes().forEach((key, value) -> log.info("attribute: {} - {}", key, value));
-              log.info("method: {}", exchange.getRequest().getMethod().name());
+              if (interceptorEnabled) {
+                exchange
+                    .getRequest()
+                    .getHeaders()
+                    .forEach((key, value) -> log.info("header: {} - {}", key, value));
+                exchange
+                    .getAttributes()
+                    .forEach((key, value) -> log.info("attribute: {} - {}", key, value));
+                log.info("method: {}", exchange.getRequest().getMethod().name());
+              }
             });
   }
 }
