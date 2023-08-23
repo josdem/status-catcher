ee8fcc117a865b610dd004ce8fd26f8d9457a42f
josdem <joseluis.delacruz@gmail.com>
2023-08-23 15:55:17 -0400

    #11 Adding swagger URL value

--- src/main/java/com/josdem/catcher/config/SwaggerConfig.java
+++ src/main/java/com/josdem/catcher/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.josdem.catcher.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +13,11 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 public class SwaggerConfig {
 
+  @Value("${swagger.server}")
+  private String swaggerServer;
+
   @Bean
   public OpenAPI customOpenAPI() {
-    return new OpenAPI().servers(Arrays.asList(new Server().url("https://catcher.josdem.io")));
+    return new OpenAPI().servers(Arrays.asList(new Server().url(swaggerServer)));
   }
 }
