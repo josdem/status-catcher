1278317b5867c4ae15ef71fe3d50e54975519e5f
josdem <joseluis.delacruz@gmail.com>
2023-08-23 14:16:38 -0400

    #10 Adding interceptor filter

---
+++ src/main/java/com/josdem/catcher/config/InterceptorFilter.java
@@ -0,0 +1,23 @@
+package com.josdem.catcher.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class InterceptorFilter implements WebFilter {
+
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+    return chain
+        .filter(exchange)
+        .doFinally(
+            request -> {
+              exchange.getAttributes().forEach((key, value) -> log.info("attribute: {} - {}", key, value));
+            });
+  }
+}
